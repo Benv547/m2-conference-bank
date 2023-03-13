271babb7e6a82a54c8db90122c14cd4b6244f64c
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-13 21:41:11 +0100

    initial commit

---
+++ src/main/java/fr/miage/M2ConferenceBankApiApplication.java
@@ -0,0 +1,13 @@
+package fr.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ConferenceBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2ConferenceBankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/M2ConferenceBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ConferenceBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
